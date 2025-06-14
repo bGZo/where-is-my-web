11b8d32ebe9a5f9175a9d1d2061eea8696b0afca
hx <im@bgzo.cc>
2025-06-14 23:36:10 +0800

    feat: update title

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {title: "😱 Where Is My Web"}
     },
     {
       path: "/about",
@@ -16,8 +17,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+      meta: {title: "About"}
     },
   ],
 });
+
+// 设置全局前置守卫
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title + " | bGZo's lab" as string
+  }
+  next()
+})
 
 export default router;
